74aba6cc299aad1a21ee82af45cfc1a13faa94fc
David <davidmontiel549@gmail.com>
2025-09-26 23:16:52 -0700

    Add Delete button functionality

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDomClient from "react-dom/client";
+import "./main.css";
+import MyApp from "./myApp";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//creat a container
+const container = document.getElementById("root");
+
+//Create root
+const root = ReactDomClient.createRoot(container);
+
+root.render(<MyApp />);
