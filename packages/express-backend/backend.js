88fbf1fbc1d3b2615464e99615c6240e875d728e
David <davidmontiel549@gmail.com>
2025-10-31 21:37:39 -0700

    Add Database access to backend
    Signed-off-by: David <davidmontiel549@gmail.com>

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,122 +1,65 @@
 import express from "express";
 import cors from "cors";
-
+import userService from "./user-service.js";
+import User from "./user-schema.js";
+//listening for items from frontend
 const app = express();
 const port = 8000;
 
-const users = {
-    users_list: [
-        {
-            id: "xyz789",
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            id: "abc123",
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            id: "ppp222",
-            name: "Mac",
-            job: "Professor"
-        },
-        {
-            id: "yat999",
-            name: "Dee",
-            job: "Aspring actress"
-        },
-        {
-            id: "zap555",
-            name: "Dennis",
-            job: "Bartender"
-        }
-    ]
-};
 app.use(cors());
 app.use(express.json());
 
-//find user by both name and job
-const findUsersbyNameAndJob =(name, job) => {
- return users["users_list"].filter((user) => user["name"] === name && user["job"] === job);
-};
-//find user by Name only
-const findUserbyName = (name) => {
-    return users["users_list"].find((user) => user["name"] === name);
-}
-const findUserById = (id) => users["users_list"].find((user) => user["id"] === id);
 
-//adding new Users
-const addUser = (user) => {
-    if (!user.hasOwnProperty("id")) {
-       generateUserId(user)
-    }
-    users["users_list"].push(user);
-
-    return user;
-}
-//find user that matches certain id
-const generateUserId = (user) => {
-    user.id = Math.floor(Math.random() * 99999);
-}
-
-//deleting Users
-/**
- * Delete a user by their id
- * @param id The id of the user to be deleted
- * @returns {boolean} `true` if user is successfully deleted and `false` user does not exist in database
- */
-const userToDelete = (id) => {
-    //check if user exist in database
-    let userToDelete = users["users_list"].find((user) => user.id === id);
-    if (userToDelete === undefined) {
-        return false;
-    }
-    users["users_list"] = users["users_list"].filter((user) => user !== userToDelete);
-    return true;
-}
-
-app.get("/users", (req, res) => {
+app.get("/users",  async(req, res) => {
     let name = req.query.name;
     let job = req.query.job;
     // if job is undefined just look for name
-    if (job === undefined ) {
-        let result = findUserbyName(name);
-        res.send(result);
+    try {
+        const result = await userService.getUsers(name, job);
+        res.send( {users_list: result})
     }
-    else if (name !== undefined) { // if both name and user are defined then look for matching job and name user
-        let result = findUsersbyNameAndJob(name, job);
-        res.send(result)
-    }
-    else { //error
-        res.code(400).send("Bad request");
+    catch (error) {
+        console.log(error);
+        res.status(500).send("An error occurred in the server.");
     }
 })
-//finding Users by id
-app.get("/users/:id", (req, res) => {
-    const id = req.params["id"]; //or req.params.id
-    let result = findUserById(id);
-    if (result === undefined) {
-        res.status(404).send("Resource not found.");
-    } else {
-        res.send(result);
-    }
-});
-app.post("/users", (req, res) => {
-    const userToAdd = req.body;
-    addUser(userToAdd)
-    res.status(201).send(userToAdd);
-});
-app.delete("/users/:id", (req, res) => {
-    const id = req.params["id"];
-    let status = userToDelete(id)
 
-    if (status) {
-        res.status(204).send("User successfully deleted");
+//finding Users by id
+app.get("/users/:id", async (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    const result = await userService.findUserById(id);
+    if (result === undefined || result === null) {
+        res.status(404).send("Resource not found.")
     }
     else {
-        res.status(404).send("resource not found");
+        res.send({ users_list: result});
     }
+});
+
+app.post("/users", async (req, res) => {
+    const userToAdd = req.body;
+    const savedUser = await userService.addUser(userToAdd);
+    if (savedUser) res.status(201).send(savedUser);
+    else res.send(500).end();
+});
+
+app.delete("/users/:id", async (req, res) => {
+    const id = req.params["id"];
+
+    try {
+        let query = await User.findByIdAndDelete(id);
+
+        if (query == null ) {
+            res.status(404).send("resource not found");
+        }
+        else {
+            res.status(204).send("User successfully deleted");
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+
 });
 
 app.listen(port, () => {
---
+++ packages/express-backend/user-schema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        job: {
+            type: String,
+            required: true,
+            trim: true,
+            validate(value) {
+                if (value.length < 2) {
+                    throw new Error("Invalid job, must be at least character.");
+                }
+            },
+        },
+    },
+    {collection: "users_list"}
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
